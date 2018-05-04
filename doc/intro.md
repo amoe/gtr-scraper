48731c4584c328812563a383bd040a52d7ceeb0f
David Banks <amoebae@gmail.com>
2018-05-04 15:06:09 +0100

    initial import

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gtr-scraper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gtr_scraper/core_test.clj
@@ -0,0 +1,7 @@
+(ns gtr-scraper.core-test
+  (:require [clojure.test :refer :all]
+            [gtr-scraper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
