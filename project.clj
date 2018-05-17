bc30f38d13c79cf7d6ef60b8820c981294f95229
David Banks <amoebae@gmail.com>
2018-05-17 17:00:32 +0100

    adding database migrations

--- project.clj
+++ project.clj
@@ -6,9 +6,14 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.8.0"]
                  [clj-http "3.9.0"]
+                 [ragtime "0.7.2"]
                  [fipp "0.6.12"]
+                 [org.postgresql/postgresql "9.4.1208.jre7"]
+                 [com.layerware/hugsql "0.4.8"]
                  [org.clojure/tools.logging "0.4.0"]
                  [ch.qos.logback/logback-classic "1.1.3"]]
+  :aliases {"migrate"  ["run" "-m" "gtr-scraper.database/migrate"]
+            "rollback" ["run" "-m" "gtr-scraper.database/rollback"]}
   :main ^:skip-aot gtr-scraper.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
