e088a548c5026a6f2ba439547e72b382cbb8ea0c
David Banks <amoebae@gmail.com>
2018-07-30 17:00:24 +0100

    new import script

--- scripts/export_to_neo4j.py
+++ scripts/export_to_neo4j.py
@@ -31,28 +31,82 @@ QUERY_PERSONS = """
     FROM person p;
 """
 
+QUERY_PROJECTS = """
+    SELECT p.id
+    FROM project p;
+"""
+
 CREATE_PERSON_NODES = """
     CREATE (p:Person {metadata})
 """
 
+CREATE_PROJECT_NODES = """
+    CREATE (p:Project {metadata})
+"""
+
+QUERY_PERSON_PROJECT_LINKS = """
+    SELECT pp.person_id, pp.project_id, pp.role_code
+    FROM person_project pp;
+"""
+
+LINK_PERSONS_WITH_PROJECTS = """
+    MATCH (p1:Person {id: {person_id}}), (p2:Project {id: {project_id}})
+    CREATE (p1)-[r:PARTICIPATED_IN {role_code: {role_code}}]->(p2);
+"""
+
+QUERY_FUNDS = """
+    SELECT f.id, f.funded_id
+    FROM fund f;
+"""
+
+CREATE_FUNDS = """
+    CREATE (f:Fund {id: {fund_id}}),
+           (p:Project {id: {project_id}})-[r:FUNDED_BY]->(f);
+"""
 
 class Neo4jExporter(object):
+    def load_person_project_links(self):
+        cur.execute(QUERY_PERSON_PROJECT_LINKS)
+        i = 1
+        for x in cur:
+            print("adding link", i, dict(x))
+            i += 1
+            with driver.session() as session:
+                with session.begin_transaction() as tx:
+                    tx.run(LINK_PERSONS_WITH_PROJECTS, dict(x))
+
     def run(self, args):
         print("hello")
         print(pg_conn)
         print(driver)
 
-
         cur.execute(QUERY_PERSONS)
-
         with driver.session() as session:
             with session.begin_transaction() as tx:
                 for x in cur:
                     tx.run(CREATE_PERSON_NODES, {'metadata': dict(x)})
 
+        cur.execute(QUERY_PROJECTS)
+        with driver.session() as session:
+            with session.begin_transaction() as tx:
+                for x in cur:
+                    tx.run(CREATE_PROJECT_NODES, {'metadata': dict(x)})
+
+
+        cur.execute(QUERY_FUNDS)
+        with driver.session() as session:
+            with session.begin_transaction() as tx:
+                for x in cur:
+                    tx.run(CREATE_FUNDS, {'fund_id': x['id'],
+                                          'project_id': x['funded_id']})
+        
+        self.load_person_project_links()
+
+        print("closing connection")
         driver.close()
+        print("connection closed")
         cur.close()
-        conn.close()
+        pg_conn.close()
             
         
 
