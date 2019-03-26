916d3c0b8896a449a957f61a616b6061cbb15541
David Banks <amoebae@gmail.com>
2019-03-26 10:42:00 +0000

    testing converter

--- test/gtr_scraper/core_test.clj
+++ test/gtr_scraper/core_test.clj
@@ -1,7 +1,11 @@
 (ns gtr-scraper.core-test
   (:require [clojure.test :refer :all]
-            [gtr-scraper.core :refer :all]))
+            [clj-time.coerce :as c]
+            [gtr-scraper.data-service :as data-service :refer [person->sql]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 4 (+ 2 2)))))
+(def sample-person-datum-from-api
+  {:created 1525197344000})
+
+(deftest person-is-loaded-with-date
+  (is (= #inst "2018-05-01T17:55:44.000-00:00" (-> (person->sql sample-person-datum-from-api nil)
+                                                   :created_date))))
